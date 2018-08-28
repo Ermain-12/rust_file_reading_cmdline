d0f8e26941340c78044c19146a2502161b97768f
Ermain <paulermain@gmail.com>
2018-08-28 18:42:24 -0400

    Refactored program with structs and implementation blocks to process user input after collecting command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
-use std::io::prelude::*;
+use std::process;
+
 
 fn main() {
     println!("Hello, world!");
@@ -8,38 +9,39 @@ fn main() {
     // Collect the command line arguments from the  user input
     let args: Vec<String> = env::args().collect();
 
-    // User specifies the query type
-    let query = &args[1];
-    let file_name = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Error parsing arugments: {:?}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {:?}", query);
-    println!("In file: {:?}", file_name);
-
-    let contents = fs::read_to_string(file_name)
-                        .expect("Error reading this file.");
-
-    println!("With text:\n {:?}", contents);
+    run(config);
 }
 
-#[warn(dead_code)]
-fn parse(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
 
-    (query, filename)
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.file_name)
+                        .expect("Error reading this file");
+
+    println!("With text: \n{:?}", contents);
 }
 
-#[warn(dead_code)]
-fn parse_config(args: &[String]) -> Config {
-    
-    let query: String = args[1].clone();
-    let filename: String = args[2].clone();
-
-    Config { query, filename }
-}
 
 #[derive(Debug)]
 struct Config {
     query: String,
-    filename: String,
+    file_name: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+
+        Ok(Config { query, file_name })
+    }
 }
